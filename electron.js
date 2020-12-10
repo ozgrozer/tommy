fd79429282ec3cc551f3d5b80a1188507a2c81ca
Ozgur Ozer <ozgr@live.com>
2020-12-11 01:06:41 +0300

    Add boilerplate

---
+++ electron.js
@@ -0,0 +1,27 @@
+const isDev = require('electron-is-dev')
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 640,
+    height: 480
+  })
+
+  if (isDev) {
+    mainWindow.loadURL('http://localhost:1240')
+  } else {
+    mainWindow.loadFile('./public/index.html')
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ src/js/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import './../css/app.scss'
+
+const App = () => {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))

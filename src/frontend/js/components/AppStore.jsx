d4f3b747f1218401022cdd1d9e9b7b3f5e3d2416
Ozgur <ozgr@live.com>
2020-12-20 00:13:38 +0300

    List apps

--- src/frontend/js/components/AppStore.jsx
+++ src/frontend/js/components/AppStore.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useContext } from 'react'
+import React, { useState, useEffect, useContext } from 'react'
 
+import apps from '~/apps.json'
 import Header from './Header'
 import { MainContext } from '~/src/frontend/js/context/MainContext'
 
@@ -10,16 +11,55 @@ const AppStore = () => {
     document.title = `App Store | ${state.appName}`
   }, [])
 
-  const searchOnChange = () => {
-    console.log('searchOnChange')
+  const [filteredApps, setFilteredApps] = useState(apps)
+  const searchOnChange = e => {
+    const searchTerm = e.target.value.toLowerCase()
+    let newApps = {}
+    if (searchTerm) {
+      for (const key in apps) {
+        const app = apps[key]
+        const found = app.n.toLowerCase().includes(searchTerm) || app.d.toLowerCase().includes(searchTerm)
+        if (found) newApps[app.id] = app
+      }
+    } else {
+      newApps = apps
+    }
+    setFilteredApps(newApps)
   }
 
   return (
     <div id='appStore'>
       <Header searchOnChange={searchOnChange} />
 
-      <div className='content'>
-        appStore
+      <div className='apps'>
+        {Object.keys(filteredApps).map(key => {
+          const app = filteredApps[key]
+          const appLogo = `https://raw.githubusercontent.com/${app.r}/master/logo.png`
+          const appAuthor = app.r.split('/')[0]
+
+          return (
+            <div key={key} className='app'>
+              <div
+                className='appLogo'
+                style={{ backgroundImage: `url(${appLogo})` }}
+              />
+
+              <div className='appDetails'>
+                <div className='appName'>
+                  {app.n}
+                </div>
+
+                <div className='appAuthor'>
+                  {appAuthor}
+                </div>
+
+                <div className='appDescription'>
+                  {app.d}
+                </div>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
   )
